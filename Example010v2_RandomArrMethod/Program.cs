5b86daf16845f95414fba6ab90fac7f281755d2d
Igor-Manko <manko161igor@gmail.com>
2022-06-30 00:30:42 +0300

    Example 010v2 поиск индекса в рандомном массиве с выводом на печать

---
+++ Example010v2_RandomArrMethod/Program.cs
@@ -0,0 +1,48 @@
+﻿// Создание методов рандомного заполнения, затем поочередного вывода массива
+
+void FillArray(int[] collection)  // метод FillArray заполнения массива аргумент collection
+{
+    int length = collection.Length;                    // задали длинну массива
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);   // до тех пор пока выполняется цикл заполняй массив рандомно
+        index++;
+    }
+}
+
+void PrintArray(int[] col)         // метод последовательной печати заполненного рандомного массива
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)   // функция поиска индекса элемента массива равный find
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;             // ставим -1 для визуального понимания в случае отсутствия результата
+    while(index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 7);
+Console.WriteLine(pos);
