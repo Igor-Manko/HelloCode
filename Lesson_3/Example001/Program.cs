e74e75ebe03a2bc70c00ad9d024b9405f4e58e0b
Igor-Manko <manko161igor@gmail.com>
2022-07-03 22:24:22 +0300

    упорядочил папки по лекциям

---
+++ Lesson_3/Example001/Program.cs
@@ -0,0 +1,55 @@
+﻿// Группы методов 
+// 1,2 ничего не вовращают, 3,4 возвращают
+// Вид 1
+
+void Method()
+{
+    Console.WriteLine("Автор...");
+}
+Method();
+
+
+// Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст сооющения");
+
+//////////
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21("Текст", 4);
+
+
+// Вид 3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
+
+// Вид 4
+string Method4(int count, string text)
+{
+    string result = string.Empty;
+
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text + " ";
+    }
+    return result;
+}
+
+string res = Method4(3, "hello");
+Console.WriteLine(res);
---
+++ Lesson_3/Example004/Program.cs
@@ -0,0 +1,40 @@
+﻿// Упорядочить массив в порядке увеличения
+
+// 6, 8, 33, 2, 11, 4, 5, 6, 7, 12, 1, 0
+
+int[] arr = { 6, 8, 33, 2, 11, 4, 5, 6, 7, 12, 1, 0 };
+
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition])
+            {
+                minPosition = j;
+            }
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
